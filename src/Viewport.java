e59f580e240109184c6ff72c7455bda3a2d2c195
csnead <csnead86@gmail.com>
2021-01-27 21:31:00 -0800

    Made Viewport instance variables final (numRows, numCols)

--- src/Viewport.java
+++ src/Viewport.java
@@ -8,8 +8,8 @@ final class Viewport
 {
    private int row;
    private int col;
-   private int numRows;
-   private int numCols;
+   private final int numRows;
+   private final int numCols;
 
    public Viewport(int numRows, int numCols)
    {
