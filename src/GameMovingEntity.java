16d4053ff6201ddda12d08f265474414805353f0
csnead <csnead86@gmail.com>
2021-03-07 20:57:12 -0800

    Updated GameMovingEntity

--- src/GameMovingEntity.java
+++ src/GameMovingEntity.java
@@ -1,4 +1,21 @@
-public class GameMovingEntity extends GameAnimatedEntity {
+import processing.core.PImage;
+
+import java.util.List;
+
+public abstract class GameMovingEntity extends GameAnimatedEntity {
     private PathingStrategy strategy;
+
+    public GameMovingEntity(String id, Point position, List<PImage> images, int actionPeriod, int animationPeriod, PathingStrategy strategy) {
+        super(id, position, images, actionPeriod, animationPeriod);
+        this.strategy = strategy;
+    }
+
+    public PathingStrategy getStrategy() {
+        return this.strategy;
+    }
+
+    protected abstract boolean moveTo(WorldModel world, GameEntity target, EventScheduler scheduler);
+
+    protected abstract Point nextPosition(WorldModel world, Point destPos);
 
 }
