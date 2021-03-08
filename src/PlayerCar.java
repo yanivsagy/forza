bbfcc85cbb767cd1f734e86507d05134bd47a50e
csnead <csnead86@gmail.com>
2021-03-07 21:22:47 -0800

    Updated PlayerCar

--- src/PlayerCar.java
+++ src/PlayerCar.java
@@ -1,2 +1,10 @@
+import processing.core.PImage;
+
+import java.util.List;
+
 public class PlayerCar extends GameAnimatedEntity {
+
+    public PlayerCar(String id, Point position, List<PImage> images, int actionPeriod, int animationPeriod) {
+        super(id, position, images, actionPeriod, animationPeriod);
+    }
 }
