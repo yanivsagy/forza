c16adbbd10c652f2da52c42da98d0c06410bee94
Yaniv Sagy <yanivsagy@gmail.com>
2021-03-07 20:46:42 -0800

    GameObstacle

--- src/NonMovingObstacle.java
+++ src/NonMovingObstacle.java
@@ -1,2 +1,12 @@
+import processing.core.PImage;
+
+import java.util.List;
+
 public class NonMovingObstacle extends GameEntity {
+
+    public static final String OBSTACLE_KEY = "obstacle";
+
+    public NonMovingObstacle(String id, Point position, List<PImage> images) {
+        super(id, position, images);
+    }
 }
--- src/OilPuddle.java
+++ src/OilPuddle.java
@@ -1,2 +1,8 @@
+import processing.core.PImage;
+import java.util.List;
+
 public class OilPuddle extends NonMovingObstacle {
+    public OilPuddle(String id, Point position, List<PImage> images) {
+        super(id, position, images);
+    }
 }
