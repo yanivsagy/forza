da4789671090b1c73f103aa67d8d9cdcec614b70
csnead <csnead86@gmail.com>
2021-03-07 20:35:59 -0800

    Updated GameEntity

--- src/GameEntity.java
+++ src/GameEntity.java
@@ -1,2 +1,52 @@
+import processing.core.PImage;
+
+import java.util.List;
+
 public class GameEntity {
+
+    abstract class Entity
+    {
+        private final String id;
+        private Point position;
+        private final List<PImage> images;
+        private int imageIndex;
+
+
+        public Entity(String id, Point position,
+                      List<PImage> images)
+        {
+            this.id = id;
+            this.position = position;
+            this.images = images;
+            this.imageIndex = 0;
+        }
+
+        protected String getID() {
+            return id;
+        }
+
+        protected List<PImage> getImages() {
+            return images;
+        }
+
+        protected Point getPosition() {
+            return position;
+        }
+
+        protected void setPosition(Point p) {
+            position = p;
+        }
+
+        public void nextImage()
+        {
+            imageIndex = (imageIndex + 1) % images.size();
+        }
+
+        public PImage getCurrentImage()
+        {
+            return images.get(this.imageIndex);
+        }
+
+    }
+
 }
