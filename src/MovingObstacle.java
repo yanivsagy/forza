313dff05c0ee2f95d384ed38b50f6e957e45996b
Yaniv Sagy <yanivsagy@gmail.com>
2021-03-07 22:03:52 -0800

    MovingObstacle

--- src/MovingObstacle.java
+++ src/MovingObstacle.java
@@ -1,2 +1,10 @@
-public class MovingObstacle extends GameMovingEntity {
+import processing.core.PImage;
+
+import java.util.List;
+
+public abstract class MovingObstacle extends GameMovingEntity {
+
+    public MovingObstacle (String id, Point position, List<PImage> images, int actionPeriod, int animationPeriod, PathingStrategy strategy) {
+        super(id, position, images, actionPeriod, animationPeriod, strategy);
+    }
 }
